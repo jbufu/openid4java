a741ca971d5951027d2840049970094b2e3f1366
zhoushuqun <zhoushuqun@0ddc078c-3e21-0410-a0b4-493e83258d60>
2008-07-04 08:25:12 +0000

    Refactor HtmlParser & YadisParser's cyber neko implementation.

--- src/org/openid4java/util/CyberNekoDOMHtmlParser.java
+++ src/org/openid4java/util/CyberNekoDOMHtmlParser.java
@@ -4,47 +4,27 @@
 
 package org.openid4java.util;
 
-import java.io.ByteArrayInputStream;
-import java.io.StringWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.List;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.xerces.xni.Augmentations;
-import org.apache.xerces.xni.QName;
-import org.apache.xerces.xni.XMLAttributes;
-import org.cyberneko.html.HTMLTagBalancingListener;
-import org.cyberneko.html.parsers.DOMParser;
+import org.apache.html.dom.HTMLDocumentImpl;
 import org.openid4java.OpenIDException;
 import org.openid4java.discovery.DiscoveryException;
 import org.openid4java.discovery.html.HtmlParser;
 import org.openid4java.discovery.html.HtmlResult;
-import org.openid4java.discovery.yadis.YadisException;
-import org.openid4java.discovery.yadis.YadisParser;
-import org.openid4java.discovery.yadis.YadisResolver;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
+import org.w3c.dom.html.HTMLHeadElement;
+import org.w3c.dom.html.HTMLLinkElement;
 
 /**
  * @author Sutra Zhou
  * 
  */
-public class CyberNekoDOMHtmlParser implements HtmlParser, YadisParser
+public class CyberNekoDOMHtmlParser implements HtmlParser
 {
-    private static Log _log = LogFactory.getLog(CyberNekoDOMHtmlParser.class);
+    private static final Log _log = LogFactory.getLog(CyberNekoDOMHtmlParser.class);
     private static final boolean DEBUG = _log.isDebugEnabled();
 
     /*
@@ -59,7 +39,8 @@ public class CyberNekoDOMHtmlParser implements HtmlParser, YadisParser
         if (DEBUG)
             _log.debug("Parsing HTML data:\n" + htmlData);
 
-        Document doc = this.parse(htmlData);
+        HTMLDocumentImpl doc = this.parseDocument(htmlData);
+
         NodeList heads = doc.getElementsByTagName("head");
         if (heads.getLength() != 1)
             throw new DiscoveryException(
@@ -67,55 +48,40 @@ public class CyberNekoDOMHtmlParser implements HtmlParser, YadisParser
                             + "found " + heads.getLength() + " : "
                             + heads.toString(),
                     OpenIDException.DISCOVERY_HTML_PARSE_ERROR);
-        Node head = heads.item(0);
-        NodeList headChildNodes = head.getChildNodes();
-        for (int i = 0, len = headChildNodes.getLength(); i < len; i++)
+
+        HTMLHeadElement head = (HTMLHeadElement) doc.getHead();
+        NodeList linkElements = head.getElementsByTagName("LINK");
+        for (int i = 0, len = linkElements.getLength(); i < len; i++)
         {
-            Node node = headChildNodes.item(i);
-            if (node.getNodeName().equalsIgnoreCase("link"))
-            {
-                String[] link = parseNodeLink(node);
-                if (link != null)
-                {
-                    setResult(link[0], link[1], result);
-                }
-            }
+            HTMLLinkElement linkElement = (HTMLLinkElement) linkElements.item(i);
+            setResult(linkElement.getRel(), linkElement.getHref(), result);
         }
 
         if (DEBUG)
             _log.debug("HTML discovery result:\n" + result);
     }
 
-    /**
-     * Parse attributes <code>rel</code> and <code>href</code> from a
-     * <code>link</code> node.
-     * 
-     * @param link
-     * @return a string array with two elements, the first element is
-     *         <code>rel</code>, the second one is <code>href</code>. If
-     *         one of <code>rel</code> and <code>href</code> is null, return
-     *         null.
-     */
-    private String[] parseNodeLink(Node link)
+    private HTMLDocumentImpl parseDocument(String htmlData) throws DiscoveryException
     {
-        String href = null, rel = null;
-        NamedNodeMap attributes = link.getAttributes();
-        if (attributes != null)
+        OpenID4JavaDOMParser parser = new OpenID4JavaDOMParser();
+        try
         {
-            Node hrefNode = attributes.getNamedItem("href");
-            if (hrefNode != null)
-            {
-                href = hrefNode.getNodeValue();
-            }
-
-            Node relNode = attributes.getNamedItem("rel");
-            if (relNode != null)
-            {
-                rel = relNode.getNodeValue();
-            }
+            parser.parse(OpenID4JavaDOMParser.createInputSource(htmlData));
         }
-        return (rel == null || href == null) ? null
-                : new String[] { rel, href };
+        catch (Exception e)
+        {
+            throw new DiscoveryException("Error parsing HTML message",
+            		OpenIDException.DISCOVERY_HTML_PARSE_ERROR, e);
+        }
+
+        if (parser.isIgnoredHeadStartElement())
+        {
+            throw new DiscoveryException(
+                    "HTML response must have exactly one HEAD element.",
+                    OpenIDException.DISCOVERY_HTML_PARSE_ERROR);
+        }
+
+        return (HTMLDocumentImpl) parser.getDocument();
     }
 
     /**
@@ -192,202 +158,6 @@ public class CyberNekoDOMHtmlParser implements HtmlParser, YadisParser
                 _log.debug("Found OpenID2 localID: " + href);
 
             result.setDelegate2(href);
-        }
-    }
-
-    /**
-     * Parse the html string into a DOM document.
-     * 
-     * @param htmlData
-     *            the html string to parse
-     * @return a doument
-     * @throws DiscoveryException
-     *             if parse failed.
-     */
-    private Document parse(String htmlData) throws DiscoveryException
-    {
-    	OpenID4JavaDOMParser parser = new OpenID4JavaDOMParser();
-        try
-        {
-            parser.parse(this.createInputSource(htmlData));
-        }
-        catch (Exception e)
-        {
-            throw new DiscoveryException("Error parsing HTML message",
-                    OpenIDException.DISCOVERY_HTML_PARSE_ERROR, e);
-        }
-
-        if (parser.isIgnoredHeadStartElement())
-        {
-            throw new DiscoveryException(
-                    "HTML response must have exactly one HEAD element.",
-                    OpenIDException.DISCOVERY_HTML_PARSE_ERROR);
-        }
-
-        return parser.getDocument();
-    }
-
-    /**
-     * Create an InputSource form a String.
-     * 
-     * @param s
-     *            the String
-     * @return an InputSource
-     * @throws NullPointerException
-     *             if s is null.
-     */
-    private InputSource createInputSource(String s)
-    {
-        try
-        {
-            return new InputSource(
-                    new ByteArrayInputStream(s.getBytes("UTF-8")));
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.openid4java.discovery.yadis.YadisParser#getHtmlMeta(java.lang.String)
-     */
-    public String getHtmlMeta(String input) throws YadisException
-    {
-        String xrdsLocation = null;
-        OpenID4JavaDOMParser parser = new OpenID4JavaDOMParser();
-        try
-        {
-            parser.parse(this.createInputSource(input));
-        }
-        catch (Exception e)
-        {
-            throw new YadisException("Error parsing HTML message",
-                    OpenIDException.YADIS_HTMLMETA_INVALID_RESPONSE, e);
-        }
-
-        if (parser.isIgnoredHeadStartElement())
-        {
-        	throw new YadisException("HTML response must have exactly one HEAD element.",
-        			OpenIDException.YADIS_HTMLMETA_INVALID_RESPONSE);
-        }
-
-        Document doc = parser.getDocument();
-        if (DEBUG)
-        {
-            _log.debug("document:\n" + toXmlString(doc));
-        }
-        NodeList heads = doc.getElementsByTagName("head");
-
-        if (heads.getLength() != 1)
-            throw new YadisException(
-                    "HTML response must have exactly one HEAD element, "
-                            + "found " + heads.getLength() + " : "
-                            + heads.toString(),
-                    OpenIDException.YADIS_HTMLMETA_INVALID_RESPONSE);
-
-        Node head = heads.item(0);
-        NodeList headChildNodes = head.getChildNodes();
-        for (int i = 0, len = headChildNodes.getLength(); i < len; i++)
-        {
-            Node node = headChildNodes.item(i);
-            if (DEBUG)
-            {
-                _log.debug("Node name: " + node.getNodeName());
-            }
-            if (node.getNodeName().equalsIgnoreCase("meta"))
-            {
-                NamedNodeMap attributes = node.getAttributes();
-                if (attributes == null)
-                {
-                    _log.debug("attributes is null.");
-                    continue;
-                }
-
-                Node httpEquivNode = attributes.getNamedItem("http-equiv");
-                if (httpEquivNode == null)
-                {
-                    _log.debug("httpEquivNode is null.");
-                    continue;
-                }
-
-                String httpEquiv = httpEquivNode.getNodeValue();
-                if (httpEquiv != null
-                        && httpEquiv
-                                .equalsIgnoreCase(YadisResolver.YADIS_XRDS_LOCATION))
-                {
-                    if (xrdsLocation != null)
-                        throw new YadisException(
-                                "More than one "
-                                        + YadisResolver.YADIS_XRDS_LOCATION
-                                        + "META tags found in HEAD: "
-                                        + head.toString(),
-                                OpenIDException.YADIS_HTMLMETA_INVALID_RESPONSE);
-
-                    xrdsLocation = attributes.getNamedItem("content")
-                            .getNodeValue();
-                    if (DEBUG)
-                        _log.debug("Found " + YadisResolver.YADIS_XRDS_LOCATION
-                                + "META tags.");
-                }
-            }
-        }
-        return xrdsLocation;
-    }
-
-    private static String toXmlString(Document doc)
-    {
-        TransformerFactory factory = TransformerFactory.newInstance();
-        Transformer transformer;
-        try
-        {
-            transformer = factory.newTransformer();
-        }
-        catch (TransformerConfigurationException e)
-        {
-            throw new RuntimeException(e);
-        }
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
-        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-
-        DOMSource source = new DOMSource(doc);
-        StringWriter xmlString = new StringWriter();
-        StreamResult streamResult = new StreamResult(xmlString);
-        try
-        {
-            transformer.transform(source, streamResult);
-        }
-        catch (TransformerException e)
-        {
-            throw new RuntimeException(e);
-        }
-        return xmlString.toString();
-    }
-}
-
-class OpenID4JavaDOMParser extends DOMParser implements HTMLTagBalancingListener
-{
-
-    private boolean ignoredHeadStartElement;
-
-    public boolean isIgnoredHeadStartElement()
-    {
-        return ignoredHeadStartElement;
-    }
-
-    public void ignoredEndElement(QName element, Augmentations augs)
-    {
-        // Do nothing.
-    }
-
-    public void ignoredStartElement(QName element, XMLAttributes attrs, Augmentations augs)
-    {
-        if (element.rawname.equals("HEAD"))
-        {
-            this.ignoredHeadStartElement = true;
         }
     }
 }
---
+++ src/org/openid4java/util/OpenID4JavaDOMParser.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2006-2008 Sxip Identity Corporation
+ */
+
+package org.openid4java.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.xerces.xni.Augmentations;
+import org.apache.xerces.xni.QName;
+import org.apache.xerces.xni.XMLAttributes;
+import org.cyberneko.html.HTMLTagBalancingListener;
+import org.cyberneko.html.parsers.DOMParser;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+/**
+ * @author Sutra Zhou
+ * 
+ */
+public class OpenID4JavaDOMParser extends DOMParser implements HTMLTagBalancingListener
+{
+    /**
+     * Create an InputSource form a String.
+     * 
+     * @param s
+     *            the String
+     * @return an InputSource
+     * @throws NullPointerException
+     *             if s is null.
+     */
+    public static InputSource createInputSource(String s)
+    {
+        try
+        {
+            return new InputSource(
+                    new ByteArrayInputStream(s.getBytes("UTF-8")));
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String toXmlString(Document doc)
+    {
+        TransformerFactory factory = TransformerFactory.newInstance();
+        Transformer transformer;
+        try
+        {
+            transformer = factory.newTransformer();
+        }
+        catch (TransformerConfigurationException e)
+        {
+            throw new RuntimeException(e);
+        }
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.STANDALONE, "yes");
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+
+        DOMSource source = new DOMSource(doc);
+        StringWriter xmlString = new StringWriter();
+        StreamResult streamResult = new StreamResult(xmlString);
+        try
+        {
+            transformer.transform(source, streamResult);
+        }
+        catch (TransformerException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return xmlString.toString();
+    }
+
+    private boolean ignoredHeadStartElement;
+
+    public boolean isIgnoredHeadStartElement()
+    {
+        return ignoredHeadStartElement;
+    }
+
+    public void ignoredEndElement(QName element, Augmentations augs)
+    {
+        // Do nothing.
+    }
+
+    public void ignoredStartElement(QName element, XMLAttributes attrs, Augmentations augs)
+    {
+        if (element.rawname.equals("HEAD"))
+        {
+            this.ignoredHeadStartElement = true;
+        }
+    }
+}
